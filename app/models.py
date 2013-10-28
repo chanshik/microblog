0578ce355eb5a84b34d7a9646f1b8f22c44ee2c1
chanshik <chanshik@gmail.com>
2013-10-28 20:11:44 +0900

    Completed - ch. 5: User Login.

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -12,6 +13,18 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.nickname
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
 
 
 class Post(db.Model):
