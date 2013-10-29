27c74877764844ed4bd0c971fdcbaae840cf2427
Chan Shik Lim <chanshik@gmail.com>
2013-10-29 23:47:58 +0900

    Merge branch 'master' of github.com:chanshik/microblog
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/models.py
+++ app/models.py
@@ -13,6 +13,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id==id),
+                               secondaryjoin=(followers.c.followed_id==id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.nickname
@@ -47,6 +53,23 @@ class User(db.Model):
 
         return new_nickname
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -55,3 +78,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % self.body
+
+
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
