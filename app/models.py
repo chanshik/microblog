62501f106246a6da5be64b19097ef99d32c3be19
Chan Shik Lim <chanshik@gmail.com>
2013-10-30 23:21:40 +0900

    Added relationship methods.

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ ROLE_USER = 0
 ROLE_ADMIN = 1
 
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -69,7 +74,6 @@ class User(db.Model):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
 
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -78,8 +82,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % self.body
-
-
-followers = db.Table('followers',
-                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
