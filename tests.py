1f67d27f8804063342dc4dcd5fc231fd4b1c97de
Chan Shik Lim <chanshik@gmail.com>
2013-10-31 23:50:29 +0900

    Add User.followed_posts() method.

--- tests.py
+++ tests.py
@@ -49,23 +49,23 @@ class TestCase(unittest.TestCase):
         assert nickname2 != nickname
 
     def test_follow(self):
-        u1 = User(nickname = 'john', email = 'john@example.com')
-        u2 = User(nickname = 'susan', email = 'susan@example.com')
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        assert u1.unfollow(u2) == None
+        assert u1.unfollow(u2) is None
         u = u1.follow(u2)
         db.session.add(u)
         db.session.commit()
-        assert u1.follow(u2) == None
+        assert u1.follow(u2) is None
         assert u1.is_following(u2)
         assert u1.followed.count() == 1
         assert u1.followed.first().nickname == 'susan'
         assert u2.followers.count() == 1
         assert u2.followers.first().nickname == 'john'
         u = u1.unfollow(u2)
-        assert u != None
+        assert u is not None
         db.session.add(u)
         db.session.commit()
         assert u1.is_following(u2) == False
